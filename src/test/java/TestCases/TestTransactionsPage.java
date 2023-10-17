c3c99c8c5b65d56e346e01968da0c1924f665f88
Diego Grisales <54412831+dfgrisales5078@users.noreply.github.com>
2023-10-17 13:50:33 -0400

    Updated transactions page tests

--- src/test/java/TestCases/TestTransactionsPage.java
+++ src/test/java/TestCases/TestTransactionsPage.java
@@ -2,7 +2,6 @@ package TestCases;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
